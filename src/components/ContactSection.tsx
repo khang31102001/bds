875ac22b398a07f39785e124bf99c99cc62b763f
Shunadesu <namp280918@gmail.com>
2025-05-26 11:11:28 +0700

    update

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -60,25 +60,22 @@ export default function ContactSection() {
         from_email: form.email,
         to_name: 'Admin',
         to_email: 'leminhkhang.ms@gmail.com',
-        message: `
-          <div style="font-family: system-ui, sans-serif, Arial; font-size: 12px">
-            <div>A message by ${form.name} has been received. Kindly respond at your earliest convenience.</div>
-            <div style="margin-top: 20px; padding: 15px 0; border-width: 1px 0; border-style: dashed; border-color: lightgrey;">
-              <table role="presentation">
-                <tr>
-                  <td style="vertical-align: top">
-                    <div style="padding: 6px 10px; margin: 0 10px; background-color: aliceblue; border-radius: 5px; font-size: 26px;" role="img">&#x1F464;</div>
-                  </td>
-                  <td style="vertical-align: top">
-                    <div style="color: #2c3e50; font-size: 16px"><strong>${form.name}</strong></div>
-                    <div style="color: #cccccc; font-size: 13px">${currentTime}</div>
-                    <p style="font-size: 16px">${form.message}</p>
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </div>
-        `
+        message: form.message,
+        time: currentTime,
+        images: [
+          {
+            url: 'https://i.imgur.com/QZxUyPk.jpg',
+            title: 'River Front Area'
+          },
+          {
+            url: 'https://i.imgur.com/nZXbSbh.jpg',
+            title: 'Hill Views Area'
+          },
+          {
+            url: 'https://i.imgur.com/QZxUyPk.jpg',
+            title: 'Farm Area'
+          }
+        ]
       },
       'a3Wq8WGDg50mE54Tg'
     )
